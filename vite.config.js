210ea71b98157640addb794160560b66dec611de
Kirill Barkov <freedomdragon777@gmail.com>
2025-03-12 18:12:15 +0400

    Обновить конфигурацию Vite для использования плагина vite-plugin-include-html; заменить контейнер для шапки на элемент include

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,17 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import includeHtml from 'vite-include-html-plugin';
 
 export default defineConfig({
+  plugins: [
+    includeHtml()
+  ],
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         introduction: resolve(__dirname, 'src/pages/introduction.html'),
       }
-    }
-  },
-  assetsInclude: ['**/*.html'],
-  optimizeDeps: {
-    esbuildOptions: {
-      logOverride: { 'commonjs-variables': 'silent' }
     }
   }
 });
